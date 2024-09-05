48a845ff6bd59d935d050e083143342624efa530
jenniferchristine <jeja2306@student.miun.se>
2024-09-05 15:48:44 +0200

    tried to insert the calculator for

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ConvertComponent } from './convert/convert.component';
@@ -12,3 +13,9 @@ export const routes: Routes = [
     { path: "404", component: NotFoundComponent }, 
     { path: "**", component: NotFoundComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
-export class ConvertComponent {
 
+export class ConvertComponent {
+  value: number | null = null;
+  fromUnit: string = "meter";
+  toUnit: string = "feet";
+  convertedValue: string = "";
+
+  convert() {
+    if (this.value === null) return;
+    let result: number;
+
+    if (this.fromUnit === "meter" && this.toUnit === "feet") {
+      result = this.value * 3.28084;
+    } else if (this.fromUnit === 'feet' && this.toUnit === 'meter') {
+      result = this.value / 3.28084;
+    } else if (this.fromUnit === 'celsius' && this.toUnit === 'fahrenheit') {
+      result = (this.value * 9/5) + 32;
+    } else if (this.fromUnit === 'fahrenheit' && this.toUnit === 'celsius') {
+      result = (this.value - 32) * 5/9;
+    } else {
+      result = this.value;
+    }
+
+    this.convertedValue = `${result} ${ this.toUnit}`;
+  }
+
+  ngOnChanges() {
+    this.convert();
+  }
 }
